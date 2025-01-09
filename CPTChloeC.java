1db7873f95641f487be45b6ddce7a5f3517d9e31
ChloeC000 <tszkiu.chui26@ycdsbk12.ca>
2025-01-08 19:17:26 -0500

    asked name of players, created and opened two quizzes txt

--- CPTChloeC.java
+++ CPTChloeC.java
@@ -4,8 +4,20 @@ public class CPTChloeC{
 	public static void main(String[]args){
 		Console con = new Console();
 	
-	con.println("hello world");
-	con.println("So sleepy");
+	String strName;
+	
+	con.println("What is your name?");
+	strName = con.readLine();
+	
+	con.println("play game");
+	con.println("view high score");
+	con.println("quit");
+	
+	
+	TextInputFile science = new TextInputFile("ScienceQuiz.txt");
+	TextInputFile movie = new TextInputFile("MovieDramaQuiz.txt");
+	
+	
 	}
 }
 
