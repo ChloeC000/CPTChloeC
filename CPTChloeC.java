fb9f4449134c09296b2e5a89dd46bf0335379300
ChloeC000 <tszkiu.chui26@ycdsbk12.ca>
2025-01-09 18:32:59 -0500

    day 2
    Made if statement for players' choices in main menu
    made variables to store players' choices, names and to read data of 3 quizzes txt file

--- CPTChloeC.java
+++ CPTChloeC.java
@@ -4,18 +4,40 @@ public class CPTChloeC{
 	public static void main(String[]args){
 		Console con = new Console();
 	
+	String strChoice1;
 	String strName;
+	String strQuiz1;
+	String strQuiz2;
+	String strQuiz3;
 	
-	con.println("What is your name?");
-	strName = con.readLine();
+	con.println("Play Game (P)");
+	con.println("View High Score (V)");
+	con.println("Quit (Q)");
 	
-	con.println("play game");
-	con.println("view high score");
-	con.println("quit");
+	strChoice1 = con.readLine();
+	if(strChoice1.equalsIgnoreCase("P")){	
+		con.println("What is your name?");
+		strName = con.readLine();
+		
+	}else if(strChoice1.equalsIgnoreCase("V")){
+		TextInputFile HightScore = new TextInputFile("HighScore.txt");
+		
+	}else if(strChoice1.equalsIgnoreCase("Q")){
+		
+	}
+
 	
 	
+	TextInputFile Quizzes = new TextInputFile("quizzes.txt");
+	strQuiz1 = con.readLine();
+	strQuiz2 = con.readLine();
+	strQuiz3 = con.readLine();
+	
+	/*
 	TextInputFile science = new TextInputFile("ScienceQuiz.txt");
 	TextInputFile movie = new TextInputFile("MovieDramaQuiz.txt");
+	TextInputFile history = new TextInputFile("HistoryQuiz.txt");
+	*/
 	
 	
 	}
